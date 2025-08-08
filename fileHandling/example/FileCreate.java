27b032e87f9957c49055fd0e8c4cf9839310ec96
Anurag Das <anurag.das2020@gift.edu.in>
2025-08-08 11:38:47 +0530

    Add files via upload

---
+++ fileHandling/example/FileCreate.java
@@ -0,0 +1,22 @@
+package com.fileHandling.example;
+
+import java.io.*;
+
+public class FileCreate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		File file = new File("text.pdf");
+
+		try {
+			if (file.createNewFile()) {
+				System.out.println("File created: " + file.getName());
+			} else {
+				System.out.println("File already exist: " + file.getName());
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
